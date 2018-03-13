99b74b74341604c1760a797019338307f8aa5101
paulonova <paulonova144@gmail.com>
2018-03-13 14:50:23 +0100

    last changes

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ var     titel;
 var     beskrivning;
 var     startDatum;
 var     slutDatum;
-var     gruppkod = 700;
+var     gruppkod;
 var     utropspris;
-
 var     countdown;
+var     giveBudButton;
 
 var budID;
 var summa;
@@ -27,39 +27,55 @@ class Auktion{
         }
 }
 
-sendRequest(AuktionUrl);
+window.onload = function(){
+    //Auktion
+    auktionID = document.getElementById("auktionID");
+    titel = document.getElementById("title");
+    beskrivning = document.getElementById("beskrivning");
+    startDatum = document.getElementById("startDatum");
+    slutDatum = document.getElementById("slutDatum");
+    gruppkod = document.getElementById("gruppkod");
+    utropspris = document.getElementById("utropspris");
 
-//Function to send a request
-function sendRequest(url){
-    fetch(url)
-    .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' + response.status);
-        return;
-      }
-      // Examine the text in the response
-      response.json()
-      .then(function(data) {
-        console.log('Status Code: ' + response.status);
-        var auktion = new Auktion(
-                    "AuktionID: " + data[0].AuktionID, 
-                    "Title: " + data[0].Titel, 
-                    "Beskrivning: " + data[0].Beskrivning, 
-                    "StartDatum: " + data[0].StartDatum, 
-                    "SlutDatum: " + data[0].SlutDatum, 
-                    "GruppKod: " + data[0].Gruppkod, 
-                    "Utropspris: " + data[0].Utropspris + " kr");
+    //Button 
+    giveBudButton = document.getElementById("give-bud");
 
-                    countdown(data[0].SlutDatum);
-                    updateAuktionCard(auktion);
-            });
+  }
+
+  sendRequest(AuktionUrl);
+
+    //Function to send a request
+    function sendRequest(url){
+        fetch(url)
+        .then(
+        function(response) {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' + response.status);
+            return;
         }
-    )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
-}
+        // Examine the text in the response
+        response.json()
+        .then(function(data) {
+            console.log('Status Code: ' + response.status);
+            var auktion = new Auktion(
+                        "<b>AuktionID: </b>" + data[0].AuktionID, 
+                        "<b>Title: </b>" + data[0].Titel, 
+                        "<b>Beskrivning: </b>" + data[0].Beskrivning, 
+                        "<b>StartDatum: </b>" + data[0].StartDatum, 
+                        "<b>SlutDatum: </b>" + data[0].SlutDatum, 
+                        "<b>GruppKod: </b>" + data[0].Gruppkod, 
+                        "<b>Utropspris: </b>" + data[0].Utropspris + " kr");
+
+                        countdown(data[0].SlutDatum);
+                        updateAuktionCard(auktion);
+                        // createNewAuktion(auktion);
+                });
+            }
+        )
+    .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+    });
+    }
 
 //Function CountDown..
 function countdown(slutDatum){
@@ -75,7 +91,7 @@ function countdown(slutDatum){
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("countdown").innerHTML = "Slut Auktion: " + days + "d " + hours + "h "
+    document.getElementById("countdown").innerHTML = "<b>Slut Auktion: </b>" + days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ";
 
     if (distance < 0) {
@@ -83,19 +99,7 @@ function countdown(slutDatum){
         document.getElementById("countdown").innerHTML = "EXPIRED";
     }
   }, 1000);
-}
-
-  window.onload = function(){
-    //Auktion
-    auktionID = document.getElementById("auktionID");
-    titel = document.getElementById("title");
-    beskrivning = document.getElementById("beskrivning");
-    startDatum = document.getElementById("startDatum");
-    slutDatum = document.getElementById("slutDatum");
-    gruppkod = document.getElementById("gruppkod");
-    utropspris = document.getElementById("utropspris");
-
-  }
+}  
 
   function updateAuktionCard(auktion){
     auktionID.innerHTML = auktion.auktionID;
@@ -103,7 +107,28 @@ function countdown(slutDatum){
     beskrivning.innerHTML = auktion.beskrivning;
     startDatum.innerHTML = auktion.startDatum;
     slutDatum.innerHTML = auktion.slutDatum;
+    gruppkod.innerHTML = auktion.gruppkod;
     utropspris.innerHTML = auktion.utropspris;
     
+  }
+
+
+
+  function createNewAuktion(auktion){
+      let div = document.createElement("DIV");
+      div.setAttribute("id", "auktion-card");
+      div.setAttribute("class", "auktion-card");
+
+      for (let i = 0; i < auktion.length; i++) {
+          
+        let pTitle = document.createElement("P");   
+        pTitle.setAttribute("id", "title");
+        let titleNode = document.createTextNode("Title: " + auktion.titel);
+        pTitle.appendChild(titleNode);
+        div.appendChild(pTitle);
+          
+      }
+      
+
   }
 
